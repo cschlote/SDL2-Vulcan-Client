894f6540921e9bd83c68da0b4ccdf74e8bf99964
Carsten Schlote <schlote@vahanus.net>
2026-05-13 17:10:59 +0200

    docs: standardize window module header

--- source/window.d
+++ source/window.d
@@ -1,4 +1,9 @@
-/** SDL window ownership wrapper with Vulkan surface creation support. */
+/** $purposeofFile
+ *
+ * Authors: Carsten Schlote, schlote@vahanus.net
+ * Copyright: Carsten Schlote, Released under CC-BY-NC-SA 4.0 license, 2018
+ * License: CC-BY-NC-SA 4.0
+ */
 module window;
 
 import bindbc.sdl : SDL_CreateWindow, SDL_DestroyWindow, SDL_GetError, SDL_GetWindowSize, SDL_SetWindowTitle, SDL_Vulkan_CreateSurface, SDL_Window, SDL_WindowFlags;
