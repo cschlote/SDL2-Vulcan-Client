615e0ee56884ff486a8d60b8018fa3184293c6fd
Carsten Schlote <schlote@vahanus.net>
2026-05-13 17:10:59 +0200

    docs: standardize version module header

--- source/version_info.d
+++ source/version_info.d
@@ -1,4 +1,9 @@
-/** Small helper that embeds the Git describe version generated at build time. */
+/** $purposeofFile
+ *
+ * Authors: Carsten Schlote, schlote@vahanus.net
+ * Copyright: Carsten Schlote, Released under CC-BY-NC-SA 4.0 license, 2018
+ * License: CC-BY-NC-SA 4.0
+ */
 module version_info;
 
 import std.string : strip;
